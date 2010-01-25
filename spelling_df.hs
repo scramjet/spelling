f84e5076c849666cdf5a3b09105538aa13b76b5a
Matthew Phillips <matt@mattp.name>
2010-01-25 15:07:43 +1030

    Updated with second version from Daniel Fischer.

--- spelling_df.hs
+++ spelling_df.hs
@@ -2,10 +2,13 @@
 module Main (main) where
 
 import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString as BS
+import Data.Bits
+import Data.Word (Word8)
 import Data.Char (toLower)
-import Data.Map (Map, findWithDefault, insertWith', member)
-import qualified Data.Map as Map (empty)
-import Data.Set as Set (Set, fromList, toList, fold, null)
+import Data.Map (Map, findWithDefault, insertWith', keysSet, empty, member)
+import qualified Data.Map as Map (lookup, empty, size)
+import Data.Set (toList, fromList)
 import Data.List (inits, tails, foldl')
 import System.Environment (getArgs)
 
@@ -13,23 +16,25 @@ dataFile = "big.txt"
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 splitWords :: B.ByteString -> [B.ByteString]
-splitWords = {-# SCC "filter" #-} filter (not . B.null) . {-# SCC "splitWith" #-} B.splitWith isNogud . {-# SCC "toLower" #-} B.map toLower
+splitWords = {-# SCC "filter" #-} filter (not . BS.null) . {-# SCC "splitWith" #-} BS.splitWith isNogud . {-# SCC "mkLow" #-} BS.map mkLow
 
-isNogud :: Char -> Bool
-isNogud c = c < 'a' || 'z' < c
+mkLow :: Word8 -> Word8
+mkLow x = x .|. 32
+
+isNogud :: Word8 -> Bool
+isNogud c = c < 97 || 122 < c
 
 train :: [B.ByteString] -> Map B.ByteString Int
 train = foldl' updateMap Map.empty
- where 
-   updateMap model word = insertWith' (+) word 1 model
+ where updateMap model word = {-# SCC "updateMap" #-} insertWith' (+) word 1 model
 
 myReadFile = B.readFile dataFile
 
 nwords :: IO (Map B.ByteString Int)
-nwords = return . train . splitWords =<< myReadFile
+nwords = (return $!) . train . splitWords =<< myReadFile
 
 edits1 :: String -> [String]
-edits1 s = toList . fromList $ deletes ++ transposes ++ replaces ++ inserts
+edits1 s = deletes ++ transposes ++ replaces ++ inserts
  where
    deletes = [a ++ bs | (a, _:bs) <- splits]
    transposes = [a ++ (b2:b1:bs) | (a, b1:b2:bs) <- splits]
@@ -38,30 +43,33 @@ edits1 s = toList . fromList $ deletes ++ transposes ++ replaces ++ inserts
    splits = zip (inits s) (tails s)
 
 correct :: Map B.ByteString Int -> String -> String
-correct wordCounts word = B.unpack . fst $ fold maxCount (B.pack "?", 0)  candidates
+correct wordCounts word = B.unpack $ maxCount (B.pack "?") 0 candidates
  where
-   candidates :: Set B.ByteString
+   candidates :: [B.ByteString]
    candidates =
-     known [word] `or` ((known $ edits1 word) `or` known_edits2 word)
+     known [word] `or` ((known e1) `or` known_edits2)
 
-   known_edits2 :: String -> Set B.ByteString
-   known_edits2 w =
-     fromList [w3 | w1 <- edits1 w, w2 <- edits1 w1
-                   , let w3 = B.pack w2, w3 `member` wordCounts]
+   e1 :: [String]
+   e1 = toList . fromList $ edits1 word
 
-   known :: [String] -> Set B.ByteString
-   known ws = fromList [w | w <- map B.pack ws, w `member` wordCounts]
+   known_edits2 :: [B.ByteString]
+   known_edits2 =
+     [w3 | w1 <- e1, w2 <- edits1 w1, let w3 = B.pack w2, w3 `member` wordCounts]
 
-   maxCount :: B.ByteString -> (B.ByteString, Int) -> (B.ByteString, Int)
-   maxCount word current@(_, currentMax)
-     | count > currentMax = (word, count)
-     | otherwise = current
-       where 
-         count = findWithDefault 1 word wordCounts
+   known :: [String] -> [B.ByteString]
+   known ws = {-# SCC "known" #-} [w | w <- map B.pack ws, w `member` wordCounts]
 
-   or :: Set B.ByteString -> Set B.ByteString -> Set B.ByteString
-   or a b | Set.null a = b
-          | otherwise = a
+   maxCount :: B.ByteString -> Int -> [B.ByteString] -> B.ByteString
+   maxCount best cmax (word:more)
+       | cmax < count  = maxCount word count more
+       | otherwise     = maxCount best cmax more
+         where
+           count = findWithDefault 1 word wordCounts
+   maxCount best _ _ = best
+
+   or :: [B.ByteString] -> [B.ByteString] -> [B.ByteString]
+   or a b | null a     = b
+          | otherwise  = a
 
 main :: IO ()
 main = do
